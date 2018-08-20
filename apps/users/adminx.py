a9b69fe06343dedc09c4b151898c092b7dc11ab2
yekai2233 <yekailovemoon@gmail.com>
2018-08-20 09:15:01 +0800

    use mx

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -2,7 +2,7 @@
 
 import xadmin
 
-from .models import EmailVerifyRecord
+from .models import EmailVerifyRecord,Banner
 from xadmin import views
 
 
@@ -15,11 +15,11 @@ class BaseSetting(object):
 # 全局修改，固定写法
 class GlobalSettings(object):
     # 修改title
-    site_title = 'meng台管理界面'
+    site_title = 'NBA后台管理界面'
     # 修改footer
-    site_footer = 'KKKBO的公司'
+    site_footer = '科比的公司'
     # 收起菜单
-    # menu_style = 'accordion'
+    menu_style = 'accordion'
 
 
 #xadmin中这里是继承object，不再是继承admin
@@ -32,17 +32,17 @@ class EmailVerifyRecordAdmin(object):
     list_filter = ['code', 'email', 'send_type', 'send_time']
 
 
-# class BannerAdmin(object):
-#     list_display = ['title', 'image', 'url','index', 'add_time']
-#     search_fields = ['title', 'image', 'url','index']
-#     list_filter = ['title', 'image', 'url','index', 'add_time']
+class BannerAdmin(object):
+    list_display = ['title', 'image', 'url','index', 'add_time']
+    search_fields = ['title', 'image', 'url','index']
+    list_filter = ['title', 'image', 'url','index', 'add_time']
 
 
 xadmin.site.register(EmailVerifyRecord,EmailVerifyRecordAdmin)
-#xadmin.site.register(Banner,BannerAdmin)
+xadmin.site.register(Banner,BannerAdmin)
 
 # 将基本配置管理与view绑定
-xadmin.site.register(views.BaseAdminView, BaseSetting)
+xadmin.site.register(views.BaseAdminView,BaseSetting)
 
 # 将title和footer信息进行注册
-xadmin.site.register(views.CommAdminView, GlobalSettings)
+xadmin.site.register(views.CommAdminView,GlobalSettings)
